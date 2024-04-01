1f1791e5819aee64d55dd07890d0de4e8b2bba9c
DELL <DELL@DELL>
2024-04-01 11:03:27 +0530

    Add project files.

---
+++ Model/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Food_API.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Catagories { get; set; }
+        public string Src { get; set; }
+    }
+}
---
+++ Model/Recipe.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Food_API.Model
+{
+    public class Recipe
+    {
+    
+
+        public int Id { get; set; }
+
+       
+        public string Name { get; set; }
+
+      
+        public string Src { get; set; }
+
+       
+        public string Category { get; set; }
+
+        public double Star { get; set; }
+
+      
+        public string Text { get; set; }
+
+        public string Preptime { get; set; }
+
+        public string Cookingtime { get; set; }
+
+      
+        public string Serving { get; set; }
+
+      
+        public List<string> Ingredients { get; set; }
+
+        public List<List<string>> NutritionFacts { get; set; }
+
+       
+        public List<string> Instructions { get; set; }
+    }
+}
